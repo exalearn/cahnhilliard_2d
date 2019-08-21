de66c9d846a081eccd57548b25ad165fa926d2f3
Thorsten Kurth <thorstenkurth@me.com>
2019-08-21 15:24:59 -0700

    made ch2d test case the same as python case (needs to be checked, first sampling tests look good)

--- cpp/src/driver.cpp
+++ cpp/src/driver.cpp
@@ -39,39 +39,81 @@ int main(int argc, char* argv[])
   
   LIKWID_MARKER_START("ch2d");
   
-  CHparamsScalar chparams;
-  SimInfo info;
+  // ********* POLYMER PARAMETERS *********
+  real Xmin     = 0.055;
+  real Xmax     = 0.5;
+  real N         = 1100;
+  real L_repeat = (1e-9) * 50.;
+  real n_repeat = 15.;
+  real L_omega  = n_repeat * L_repeat;
+  real L_kuhn   = (1e-9) * 1.75;
+    // **************************************
   
   // *********  Inputs  ***********
+  //SimInfo
+  SimInfo info;
   info.nx               = 128;
   info.ny               = 128;
   info.dx               = 1.0 / info.nx;
   info.dy               = 1.0 / info.ny;
   info.t0               = 0.0;
-  info.bc               = "periodic";
-  info.rhs_type         = "ch_non_thermal";
-
-  real eps_2          = pow( 0.01 ,2 );
-  chparams.eps_2        = eps_2;
-  chparams.b            = eps_2 / info.dx / info.dx;
-  chparams.u            = eps_2 / info.dx / info.dx;
-  chparams.sigma        = eps_2 / info.dx / info.dx / info.dx / info.dx / 200.0;
-  chparams.m            = 0.0;
-  chparams.sigma_noise  = 0.0;
+  info.bc               = "neumann";
+  info.rhs_type         = "ch_thermal_no_diffusion";
   
-  int n_tsteps        = 25;
-  real n_dt         = 300.0;
+  //CHParams
+  CHparamsVector chparams(info.nx, info.ny);
+  chparams.sigma_noise  = 0.0;
+  chparams.eps2_min     = 0.0;
+  chparams.eps2_max     = 1.0;
+  chparams.sigma_min    = 0.0;
+  chparams.sigma_max    = 1.0e10;
+  chparams.T_min        = 0.1;
+  chparams.T_max        = 1.0;
+  chparams.L_kuhn       = L_kuhn;
+  chparams.N            = N;
+  chparams.L_omega      = L_omega;
+  chparams.X_min        = Xmin;
+  chparams.X_max        = Xmax;
+  
+  int n_tsteps        = 100;
+  real n_dt         = 2000.0;
   // ******************************
-
+  
+  //fill vectors
+  for(unsigned int i=0; i<info.nx*info.ny; i++){
+    chparams.b[i] = 1.;
+    chparams.u[i] = 1.;
+    chparams.m[i] = 0.;
+    chparams.T_const[i] = chparams.T_max;
+  }
+  chparams.compute_and_set_eps2_and_sigma_from_polymer_params( chparams.T_max , info );
+  
+  real eps_2_max = chparams.eps_2[0];
+  real u_max = chparams.u[0];
+  real b_max = chparams.b[0];
+  real sigma_max = chparams.sigma[0];
+  for(unsigned int i=1; i<info.nx*info.ny; i++){
+    eps_2_max = std::max(eps_2_max, chparams.eps_2[i]);
+    u_max = std::max(u_max, chparams.u[i]);
+    b_max = std::max(b_max, chparams.b[i]);
+    sigma_max = std::max(sigma_max, chparams.sigma[i]);
+  }
+  
   // Compute linear timescales
-  real dt_biharm  = (info.dx * info.dx * info.dx * info.dx) / chparams.eps_2;
-  real dt_diff    = info.dx * info.dx / chparams.u;
-  real dt_lin     = 1.0 / chparams.sigma;
+  real dt_biharm  = (info.dx * info.dx * info.dx * info.dx) / eps_2_max;
+  real dt_diff    = info.dx * info.dx / std::max(u_max, b_max);
+  real dt_lin     = 1.0 / sigma_max;
+  real dt_stiff   = std::min(std::min(dt_biharm, dt_diff), dt_lin);
 
   // Setup temporal checkpointing
-  real tf         = n_dt * dt_biharm;
+  real tf         = n_dt * dt_stiff;
   real dt_check   = tf / n_tsteps;
-
+  
+  //temperatures
+  aligned_vector<double> T(n_tsteps);
+  for(int t=0; t<n_tsteps/4; t++) T[t] = chparams.T_max;
+  for(int t=0; t<n_tsteps/4; t++) T[t+n_tsteps/4] = (chparams.T_max*(float(n_tsteps/4.)-1.-t)+chparams.T_min*t)/(float(n_tsteps)/4.-1.);
+  for(int t=n_tsteps/4; t<n_tsteps; t++) T[t] = chparams.T_min;
 
   std::cout << "Biharmonic timescale dt_biharm = " << dt_biharm << std::endl;
   std::cout << "Diffusion timescale dt_diff = " << dt_diff/dt_biharm << " dt_biharm" << std::endl;
@@ -82,9 +124,12 @@ int main(int argc, char* argv[])
   
   // Run solver
   for (int i=0; i<n_tsteps; i++) {
-    info.t0 = i * dt_check;
-    info.tf = (i+1) * dt_check;
-    std::cout << "t0 = " << info.t0/dt_biharm << " dt_biharm , tf = " << info.tf/dt_biharm << " dt_biharm" << std::endl;
+    info.t0 = i * dt_stiff;
+    info.tf = (i+1) * dt_stiff;
+    std::cout << "t0 = " << info.t0/dt_lin << " dt_lin , tf = " << info.tf/dt_lin << " dt_lin" << std::endl;
+    for(unsigned int k=0; k<info.nx*info.ny; k++){
+      chparams.T_const[k] = T[i];
+    }
     run_solver.start();
     run_ch_solver(chparams , info);
     run_solver.stop();
